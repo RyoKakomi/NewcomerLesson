a9c09c3eb56d5f9afee6d99d6dae7f73a6db8fa4
PC20104\d_aka <aki.shogun@gmail.com>
2020-08-05 10:57:05 +0900

    3問目の内容作成

---
+++ Lesson3/question.js
@@ -0,0 +1,11 @@
+function solution(number){
+  //ここにコードを記入してください。
+  
+}
+
+
+/* 以下はテストコードになるのでいじらないでください。 */
+console.log(solution(10) == 23);
+console.log(solution(20) == 78);
+console.log(solution(63) == 870);
+console.log(solution(120) == 3300);
