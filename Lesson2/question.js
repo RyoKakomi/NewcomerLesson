2d5f06c304077d18665b5304d65fb25d2f09438f
micro38kiri <aki.shogun@gmail.com>
2020-08-05 09:53:40 +0900

    ２問目のコード

---
+++ Lesson2/question.js
@@ -0,0 +1,14 @@
+function getCount(str) {
+  var vowelsCount = 0;
+  
+  // ここにコードを追加してください。
+  
+  return vowelsCount;
+}
+
+
+/* 以下はテストコードになるのでいじらないでください。 */
+console.log(getCount("abracadabra") == 5);
+console.log(getCount("konnnitiwa") == 4);
+console.log(getCount("motimotinoki") == 6);
+console.log(getCount("aomorihachinohe") == 8);
