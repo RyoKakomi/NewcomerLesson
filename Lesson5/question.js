36edd7dc574afdba18d02a216afc780cdca34855
PC20104\d_aka <aki.shogun@gmail.com>
2020-08-05 15:27:35 +0900

    5問目作成

---
+++ Lesson5/question.js
@@ -0,0 +1,10 @@
+function digital_root(n) {
+    // ここにコードを記述してください。
+}
+
+
+/* 以下はテストコードになるのでいじらないでください。 */
+console.log(digital_root(16) == 7);
+console.log(digital_root(942) == 6);
+console.log(digital_root(456) == 6);
+console.log(digital_root(132189) == 6);
