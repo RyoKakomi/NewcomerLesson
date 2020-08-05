46ed050a8a6c3e985caf83c3336425cfa18a09d1
PC20104\d_aka <aki.shogun@gmail.com>
2020-08-05 14:59:36 +0900

    4問目作成

---
+++ Lesson4/question.js
@@ -0,0 +1,9 @@
+function even_or_odd(number) {
+    // ここにコードを記述してください。
+  }
+
+/* 以下はテストコードになるのでいじらないでください。 */
+console.log(even_or_odd(2), "偶数")
+console.log(even_or_odd(0), "偶数")
+console.log(even_or_odd(7), "奇数")
+console.log(even_or_odd(1), "奇数")
